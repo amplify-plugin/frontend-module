a6221f55a4bd61ddbb970178cc0132bb85e9b09c
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-10-09 19:31:51 +0600

    ascii letter validation added

--- Http/Requests/Auth/LoginRequest.php
+++ Http/Requests/Auth/LoginRequest.php
@@ -28,7 +28,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'string', 'email:rfc,dns', 'max:255', 'exists:contacts,email'],
+            'email' => ['required', 'string', 'email:rfc,dns', 'max:255', 'exists:contacts,email', 'ascii'],
             'password' => ['required', 'string', 'min:'.SecurityHelper::passwordLength(), 'max:255'],
         ];
     }
