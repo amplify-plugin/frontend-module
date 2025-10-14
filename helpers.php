9947ee030cd965aa8a6d76e71475ff751f8491b7
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-10-15 04:07:49 +0600

    product price and product availability widget added

--- helpers.php
+++ helpers.php
@@ -2,12 +2,12 @@
 
 if (!function_exists('product_not_avail_message')) {
     function product_not_avail_message() {
-        return config('amplify.frontend.product_not_available_text', 'Call for Pricing');
+        return trans(config('amplify.frontend.product_not_available_text', 'Call for Pricing'));
     }
 }
 
 if (!function_exists('product_out_stock_message')) {
     function product_out_stock_message() {
-        return config('amplify.frontend.product_available_text', 'Available in 3-5 days');
+        return trans(config('amplify.frontend.product_available_text', 'Available in 3-5 days'));
     }
 }
