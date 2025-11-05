ff1f841ec708850ce9b7c99c709fead258af1205
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-11-06 05:23:01 +0600

    customer part add and delete on erp done

--- Http/Controllers/CustomerPartNumberController.php
+++ Http/Controllers/CustomerPartNumberController.php
@@ -2,9 +2,11 @@
 
 namespace Amplify\Frontend\Http\Controllers;
 
+use Amplify\ErpApi\Facades\ErpApi;
 use Amplify\Frontend\Http\Requests\CustomerPartNumberRequest;
 use Amplify\System\Backend\Models\CustomPartNumber;
 use Amplify\System\Backend\Models\Event;
+use Amplify\System\Backend\Models\Product;
 use Amplify\System\Factories\NotificationFactory;
 use App\Http\Controllers\Controller;
 
@@ -15,9 +17,19 @@ class CustomerPartNumberController extends Controller
         try {
             $inputs = $request->validated();
 
+            $product = Product::findOrFail($inputs['product_id']);
+
             $customerPartNumber = CustomPartNumber::where('customer_id', $inputs['customer_id'])
                 ->where('product_id', $inputs['product_id'])
                 ->first();
+
+            $payloads = [
+                'customer_number' => customer()->erp_id,
+                'customer_product_code' => $inputs['customer_product_code'],
+                'item_number' => $product->product_code,
+                'item_uom' => $inputs['customer_product_uom'],
+                'action' => 'change'
+            ];
 
             if ($customerPartNumber) {
                 $customerPartNumber->update($inputs);
@@ -34,6 +46,8 @@ class CustomerPartNumberController extends Controller
                 NotificationFactory::call(Event::CUSTOMER_PART_NUMBER_DELETED, $customerPartNumber->toArray());
             }
 
+            ErpApi::createUpdateCustomerPartNumber($payloads);
+
             return response()->json(['message' => $message]);
         } catch (\Exception $exception) {
             return response()->json(['message' => $exception->getMessage()], 500);
@@ -49,14 +63,22 @@ class CustomerPartNumberController extends Controller
             $customerPartNumber = CustomPartNumber::where('customer_id', $inputs['customer_id'])
                 ->where('product_id', $inputs['product_id'])
                 ->first();
-            if ($customerPartNumber) {
 
+            if ($customerPartNumber) {
                 $data = $customerPartNumber->toArray();
+                NotificationFactory::call(Event::CUSTOMER_PART_NUMBER_DELETED, $data);
+
+                ErpApi::createUpdateCustomerPartNumber([
+                    'customer_number' => customer()->erp_id,
+                    'customer_product_code' => $inputs['customer_product_code'],
+                    'item_number' => $customerPartNumber->product->product_code,
+                    'item_uom' => $customerPartNumber->customer_product_uom,
+                    'action' => 'delete'
+                ]);
 
                 $customerPartNumber->delete();
-
-                NotificationFactory::call(Event::CUSTOMER_PART_NUMBER_DELETED, $data);
             }
+
             return response()->json(['message' => __('Customer part number removed successfully.')]);
         } catch (\Exception $exception) {
             return response()->json(['message' => $exception->getMessage()], 500);
