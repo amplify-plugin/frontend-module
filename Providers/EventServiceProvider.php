29a4a9814474ab6b34e130ebd5135615ce57d8c1
Sahadat <sahadatsays@gmail.com>
2025-11-25 21:37:43 +0600

    Fix: => update 'clean_string' validation rule to allow additional characters

--- Providers/EventServiceProvider.php
+++ Providers/EventServiceProvider.php
@@ -34,7 +34,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         Validator::extend('clean_string', function ($attribute, $value, $parameters, $validator) {
-            return preg_match('/^[\pL\pN\s\'-]+$/u', $value);
+            return preg_match('/^[\pL\pN\s\'\-\+\(\)]+$/u', $value);
         });
 
         Validator::replacer('clean_string', function ($message, $attribute, $rule, $parameters) {
