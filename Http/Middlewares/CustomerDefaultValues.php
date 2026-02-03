cac475c4dda6bdf63d7337151b761a99710b6661
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-02-03 22:23:05 +0600

    :edit: skipping default ship to is existing

--- Http/Middlewares/CustomerDefaultValues.php
+++ Http/Middlewares/CustomerDefaultValues.php
@@ -20,30 +20,33 @@ class CustomerDefaultValues
     {
 
         if (!$request->session()->has('ship_to_address')) {
+
             $contact = $request->user('customer');
+
             $customer = $contact->customer;
-
-
-            $defaultAddress = CustomerAddress::where('customer_id', $customer->getKey())->where(function ($query) use ($contact, $customer) {
-                $query->where('id', is_numeric($contact->customer_address_id) ? $contact->customer_address_id : $customer->default_address_id)
-                    ->orWhere('address_code', $customer->shipto_address_code);
-            })->first();
-
 
             $contactLogin = ContactLogin::firstOrCreate(['customer_id' => $customer->getkey(), 'contact_id' => $contact->getKey(), 'active' => true]);
 
-            if ($defaultAddress) {
-                $contactLogin->customer_address_id = $defaultAddress->getKey();
-                $contactLogin->ship_to_name = $defaultAddress->address_code;
-                $contactLogin->save();
+            if (!empty($customer->shipto_address_code)) {
+                $defaultAddress = CustomerAddress::where('customer_id', $customer->getKey())->where(function ($query) use ($contact, $customer) {
+//                $query->where('id', is_numeric($contact->customer_address_id) ? $contact->customer_address_id : $customer->default_address_id)
+                    $query->where('address_code', $customer->shipto_address_code);
+                })->first();
 
-                $attr = $defaultAddress->toArray();
-                $attr['allow_backorder'] = $customer->allow_backorder ?? false;
-                $attr['business_contact'] = $customer->business_contact ?? null;
-                $attr['customer_po_required'] = $customer->customer_po_required ?? false;
-                $attr['carrier_code'] = $customer->carrier_code ?? null;
 
-                $request->session()->put('ship_to_address', ErpApi::init('default')->adapter()->renderSingleCustomerShippingLocation($attr)->toArray());
+                if ($defaultAddress) {
+                    $contactLogin->customer_address_id = $defaultAddress->getKey();
+                    $contactLogin->ship_to_name = $defaultAddress->address_code;
+                    $contactLogin->save();
+
+                    $attr = $defaultAddress->toArray();
+                    $attr['allow_backorder'] = $customer->allow_backorder ?? false;
+                    $attr['business_contact'] = $customer->business_contact ?? null;
+                    $attr['customer_po_required'] = $customer->customer_po_required ?? false;
+                    $attr['carrier_code'] = $customer->carrier_code ?? null;
+
+                    $request->session()->put('ship_to_address', ErpApi::init('default')->adapter()->renderSingleCustomerShippingLocation($attr)->toArray());
+                }
             }
         }
 
