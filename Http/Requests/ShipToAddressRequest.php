5112727b7cfab28d9c7251beaf3f60d1af64dd4a
arifeasyask <ariful.islam@easyask.com>
2026-02-09 00:40:15 +0600

    :up: Update: => Removed address code input and add address code unique check functionality

--- Http/Requests/ShipToAddressRequest.php
+++ Http/Requests/ShipToAddressRequest.php
@@ -33,7 +33,7 @@ class ShipToAddressRequest extends FormRequest
         $rules = [
             'customer_id' => 'integer|exists:customers,id',
             'address_name' => 'required|string|max:255',
-            'address_code' => ['required', 'string', 'max:255'],
+            //'address_code' => ['required', 'string', 'max:255'],
             'address_1' => 'required|string|max:255',
             'address_2' => 'nullable|string|max:255',
             'address_3' => 'nullable|string|max:255',
@@ -43,15 +43,15 @@ class ShipToAddressRequest extends FormRequest
             'country_code' => 'nullable|string|size:2',
         ];
 
-        if ($this->method() == 'PUT') {
-            $rules['address_code'][] = Rule::unique('customer_addresses', 'address_code')->where(function ($query) {
-                return $query->where('customer_id', customer()->getKey());
-            })->ignore($this->route('address'));
-        } else {
-            $rules['address_code'][] = Rule::unique('customer_addresses', 'address_code')->where(function ($query) {
-                return $query->where('customer_id', customer()->getKey());
-            });
-        }
+        // if ($this->method() == 'PUT') {
+        //     $rules['address_code'][] = Rule::unique('customer_addresses', 'address_code')->where(function ($query) {
+        //         return $query->where('customer_id', customer()->getKey());
+        //     })->ignore($this->route('address'));
+        // } else {
+        //     $rules['address_code'][] = Rule::unique('customer_addresses', 'address_code')->where(function ($query) {
+        //         return $query->where('customer_id', customer()->getKey());
+        //     });
+        // }
 
         return $rules;
     }
