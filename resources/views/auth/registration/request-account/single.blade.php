1f0048275ebcc5e5d6d25c9114d9114c5a148248
Sahadat <sahadatsays@gmail.com>
2026-04-06 14:37:25 +0600

    Fix => Update customer code and and contact number in single.blade.php file.

--- resources/views/auth/registration/request-account/single.blade.php
+++ resources/views/auth/registration/request-account/single.blade.php
@@ -4,7 +4,7 @@
     </div>
     <div class="col-md-6">
         <input type="hidden" name="required[]" value="customer_account_number">
-        {!! \Form::rText('customer_account_number', 'Account Number', null, true, ['placeholder' => 'Account Number']) !!}
+        {!! \Form::rText('customer_account_number', 'Customer Code', null, true, ['placeholder' => 'Enter valid customer code...']) !!}
     </div>
     <div class="col-md-6">
         {!! \Form::rEmail('contact_email', 'Email', null, true, ['placeholder' => 'Email Address']) !!}
@@ -13,31 +13,49 @@
         </small>
     </div>
     <div class="col-md-6">
-        {!! \Form::rPassword('contact_password', 'Password', true, ['placeholder' => 'Enter Password', 'title'=>"Minimum 8 character required with at least one Number, one lower case, one upper case letter and special characters(#?!@$%^&amp;*)."]) !!}
+        {!! \Form::rTel('contact_phone_number', trans('Phone'), null, true, [
+            'placeholder' => 'Enter Contact Phone Number',
+            'tabindex' => 6,
+        ]) !!}
     </div>
     <div class="col-md-6">
-        {!! \Form::rPassword('contact_password_confirmation', 'Retype Password', true, ['placeholder' => 'Enter Password']) !!}
+        {!! \Form::rPassword('contact_password', 'Password', true, [
+            'placeholder' => 'Enter Password',
+            'title' =>
+                "Minimum 8 character required with at least one Number, one lower case, one upper case letter and special characters(#?!@$%^&amp;*).",
+        ]) !!}
+    </div>
+    <div class="col-md-6">
+        {!! \Form::rPassword('contact_password_confirmation', 'Retype Password', true, [
+            'placeholder' => 'Enter Password',
+        ]) !!}
     </div>
 </div>
 <div class="row">
-    {{ $slot  }}
+    {{ $slot }}
 </div>
 <div class="row">
     <div class="col-md-6">
-        @if($newsletterSubscription)
-            {!! \Form::rCheckbox('contact_newsletter','', ['yes' => config('app.name').' newsletter subscription.']) !!}
+        @if ($newsletterSubscription)
+            {!! \Form::rCheckbox('contact_newsletter', '', ['yes' => config('app.name') . ' newsletter subscription.']) !!}
         @endif
     </div>
     <div class="col-md-6">
-        @if($acceptTermsConfirmation)
-            <input type="hidden" name="required[]" value="contact_accept_term"/>
-            {!! \Form::rCheckbox('contact_accept_term','', ['yes' => 'I accept the '.config('app.name').' Terms and Conditions.'], [], true) !!}
+        @if ($acceptTermsConfirmation)
+            <input type="hidden" name="required[]" value="contact_accept_term" />
+            {!! \Form::rCheckbox(
+                'contact_accept_term',
+                '',
+                ['yes' => 'I accept the ' . config('app.name') . ' Terms and Conditions.'],
+                [],
+                true,
+            ) !!}
         @endif
     </div>
-    @if($captchaVerification)
+    @if ($captchaVerification)
         <div class="col-md-12">
-            <x-captcha :display="$captchaVerification" id="captcha-container-account"
-                       field-name="contact_captcha" :reload-captcha="$active" />
+            <x-captcha :display="$captchaVerification" id="captcha-container-account" field-name="contact_captcha"
+                :reload-captcha="$active" />
         </div>
     @endif
 </div>
