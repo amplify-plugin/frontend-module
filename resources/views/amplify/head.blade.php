afee1f72fdccbe4fe25b7094e1512186d1aa9033
Mohammad Hafijul Islam <hafijul233@gmail.com>
2026-05-28 01:35:20 +0600

    google tag manager code added

--- resources/views/amplify/head.blade.php
+++ resources/views/amplify/head.blade.php
@@ -8,23 +8,8 @@
 @stack('custom-style')
 @stack('internal-style')
 @stack('head-script')
+
 <x-google-analytic/>
+
 <x-site.script-manager position="header"/>
-{{--
-<style>
-    :root {
-        --blue: {{ theme_option('primary_color', null, '#007bff') }};
-        --red: {{ theme_option('danger_color', null, '#dc3545') }};
-        --yellow: {{ theme_option('warning_color', null, '#ffc107') }};
-        --green: {{ theme_option('success_color', null, '#28a745') }};
-        --cyan: {{ theme_option('info_color', null, '#17a2b8') }};
-        --primary: {{ theme_option('primary_color', null, '#007bff') }};
-        --secondary: {{ theme_option('secondary_color', null, '#6c757d') }};
-        --success: {{ theme_option('success_color', null, '#28a745') }};
-        --info: {{ theme_option('info_color', null, '#17a2b8') }};
-        --warning: {{ theme_option('warning_color', null, '#ffc107') }};
-        --danger: {{ theme_option('danger_color', null, '#dc3545') }};
-        --light: {{ theme_option('light_color', null, '#f8f9fa') }};
-        --dark: {{ theme_option('dark_color', null, '#343a40') }};
-    }
-</style>--}}
+
